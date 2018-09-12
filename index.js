5f2ddaf8c6098809b8430d653603ff93d786b7f6
wickning1 <git@mythgaming.net>
2018-09-12 14:29:13 -0500

    reworked the basic structure of the app to promote code re-use between node API services that we prop up

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
-const express = require('express')
-require('express-async-errors')
-const app = express()
-const mongoose = require('mongoose')
-const logger = require('morgan')
+const service = require('./lib/apiservice')
 const helpers = require('./lib/helpers')
 
-// database config
-var db_host = process.env.DB_HOST || 'localhost';
-var db_port = process.env.DB_PORT || '27017';
-var db_authdb = process.env.DB_AUTHDATABASE || '';
-var db_user = process.env.DB_USER || '';
-var db_pw = process.env.DB_PASSWORD || '';
-var db_name = process.env.DB_DATABASE || 'search-featured-results';
-var db_userpassword_prefix = '';
-if (db_user.length > 0 && db_pw.length > 0) db_userpassword_prefix = db_user+':'+db_pw+'@';
-var db_authdb_suffix = '';
-if (db_authdb.length > 0) db_authdb_suffix = '?authSource='+db_authdb;
-
-// server config
-var server_port = parseInt(process.env.PORT, 10) || 3000;
+var app = service.app
 
 // models
 var Result = require('./models/result')
-
-// middleware
-app.use(logger('tiny'))
-app.use(express.json())
 
 // add endpoints
 app.get('/search', async function (req, res) {
@@ -68,23 +47,4 @@ app.delete('/result/:id', async function (req, res) {
   res.sendStatus(200)
 })
 
-// start up
-var db_connect = function () {
-  mongoose.connect('mongodb://'+db_userpassword_prefix+db_host+':'+db_port+'/'+db_name+db_authdb_suffix, {
-    ssl: process.env.DB_SSL == 'true' ? true : false,
-    reconnectTries: Number.MAX_VALUE,
-    reconnectInterval: 500,
-    poolSize: 50,
-    useNewUrlParser: true
-  })
-  .then(function () {
-    console.log("DB connection alive")
-    app.listen(server_port)
-  })
-  .catch(function (err) {
-    console.log(err)
-    console.log('trying again in 5 seconds')
-    setTimeout(db_connect, 5000)
-  })
-}
-db_connect()
+service.start()
