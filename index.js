f5b89d080d55054994fbaf14811d718c1e854eb7
BlueDreaux <Drew.Pilling@GMail.com>
2022-04-28 17:20:41 -0500

    Linting made some good suggestions for changes.

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.get('/peoplesearch', async function (req, res) {
     db.getall(listingSQL, whereClause.binds)
   ])
   // eslint-disable-next-line no-return-assign
-  people.map(person => { delete person.plid && Object.keys(person).forEach(property => person[property] = (person[property] ? person[property].toString() : '')) })
+  people.forEach(person => { delete person.plid && Object.keys(person).forEach(property => person[property] = (person[property] ? person[property].toString() : '')) })
   response.count = hitCount
   response.lastpage = Math.ceil(response.count / params.n)
   response.results = people
@@ -75,27 +75,27 @@ app.get('/departments', async function (req, res) {
 })
 // ====================================================================================================================================
 app.get('/search', async function (req, res) {
-  var query = req.query.q
+  const query = req.query.q
   if (query && query.length > 1024) return
-  var asyoutype = !!req.query.asyoutype
-  var results = await Result.findByQuery(query)
-  var ret = results.map(result => result.basic())
+  const asyoutype = !!req.query.asyoutype
+  const results = await Result.findByQuery(query)
+  const ret = results.map(result => result.basic())
   res.json(ret)
   if (!asyoutype) Query.record(query, results)
 })
 app.get('/adminsearch', authorize, async function (req, res) {
-  var query = req.query.q
+  const query = req.query.q
   if (query && query.length > 1024) return res.status(400).send('Query length is limited to 1kB.')
-  var results = await Result.findByQuery(query)
-  var ret = results.map(result => result.basicPlusId())
+  const results = await Result.findByQuery(query)
+  const ret = results.map(result => result.basicPlusId())
   res.json(ret)
 })
 app.get('/results', authorize, async function (req, res) {
-  var ret = (await Result.getAllWithQueries()).map(result => { return result.fullWithCount() })
+  const ret = (await Result.getAllWithQueries()).map(result => { return result.fullWithCount() })
   res.json(ret)
 })
 app.post('/result', authorize, async function (req, res) {
-  var input = req.body
+  const input = req.body
   if (!input) return res.status(400).send('POST body was not parseable JSON.')
   if (util.isBlank(input.url)) return res.status(400).send('Posted result must contain a URL.')
 
@@ -122,13 +122,13 @@ app.post('/result', authorize, async function (req, res) {
 })
 app.get('/result/:id', authorize, async function (req, res) {
   if (!util.isHex(req.params.id)) return res.status(400).send('Bad id format. Should be a hex string.')
-  var result = await Result.getWithQueries(req.params.id)
+  const result = await Result.getWithQueries(req.params.id)
   res.json(result.full())
 })
 app.put('/result/:id', authorize, async function (req, res) {
   if (!util.isHex(req.params.id)) return res.status(400).send('Bad id format. Should be a hex string.')
   if (!req.body) return res.status(400).send('POST body was not parseable JSON.')
-  var result = await Result.findById(req.params.id)
+  const result = await Result.findById(req.params.id)
   if (!result) return res.status(404).send('That result id does not exist.')
   result.fromJson(req.body)
   if (!result.valid()) return res.status(400).send('Result did not validate.')
