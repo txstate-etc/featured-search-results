f89e074366a15cf2c83c35a3c886dc5f54e36a77
Nick Wing <nwing@txstate.edu>
2022-06-22 05:37:53 -0500

    exit all containers after a test run

--- test.bash
+++ test.bash
@@ -3,9 +3,9 @@ if [ $# -eq 0 ]; then
   # --attach to suppress logging output from other containers.
   docker compose -f docker-compose.test.yml \
                  -f docker-compose.override.yml \
-                 up --build --force-recreate --remove-orphans --attach search-featured-results-test
+                 up --build --force-recreate --remove-orphans --abort-on-container-exit --attach search-featured-results-test
 else
-  # We were passed one or more arguemnts. Interpret that to mean someone wanted response time logging so don't uncomment logging off directives from override.
+  # We were passed one or more arguments. Interpret that to mean someone wanted response time logging so don't uncomment logging off directives from override.
   docker compose -f docker-compose.test.yml \
                  -f docker-compose.override.yml \
                  up --build --force-recreate --remove-orphans --abort-on-container-exit --exit-code-from search-featured-results-test
