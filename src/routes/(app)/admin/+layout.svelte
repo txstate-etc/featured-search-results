bbea4c41091758cd2272c3790cee8780cb5b7954
Drew Pilling (aap64) <Drew.Pilling@TxState.edu>
2023-10-09 11:12:24 -0500

    Merging change differences and correcting npm dependencies of our custom svelte component libraries.

--- src/routes/(app)/admin/+layout.svelte
+++ src/routes/(app)/admin/+layout.svelte
@@ -1,3 +1,27 @@
+<script lang="ts">
+  import { afterNavigate, goto } from '$app/navigation'
+  import { base } from '$app/paths'
+
+  export let data: { isEditor?: boolean, login?: string }
+
+  afterNavigate(() => {
+    // make sure we're still logged in, and as the same user
+    // if not, refresh the page so that our code in +layout.server.ts kicks in
+    fetch(`${base}/self`).then(async resp => {
+      const { login, isEditor } = await resp.json() as { login?: string, isEditor?: boolean }
+      if (!login || !isEditor || login !== data.login) location.reload()
+    }).catch(() => { location.reload() })
+  })
+
+  async function onLogout () {
+    // this is a button with an on:click instead of a link with an href
+    // because it's generally bad practice to change the user's state with
+    // a link (e.g. browser accelerators sometimes pre-load link targets in
+    // the background which would log the user out)
+    await goto(base + '/logout')
+  }
+</script>
+
 <header>
   <title>Search Featured Results Admin</title>
   <nav class='headerNav'>
@@ -6,7 +30,7 @@
     <a class='homelink' href='./search'>Featured Search</a>
     <a href='TODO'>Add Featured Search Result</a>
     <a href='TODO'>Visitor Searches</a>
-    <a href='TODO'>Logout</a>
+    <button type="button" on:click={onLogout}>Logout</button>
   </nav>
 </header>
 
