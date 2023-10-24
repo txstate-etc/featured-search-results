91958b58e1d8e4392dcb9d689022595cf22c6df9
Drew Pilling (aap64) <Drew.Pilling@TxState.edu>
2023-10-24 12:23:48 -0500

    Updated ResponsiveTable to allow consumers to bind a callback function that will determine what fields will nest based on whatever introspeciton of the data they want to write. Useful for overwriting default nesting behavior on ALL objects and arrays by simply returning a string array of the properties that they want on separate rows, or performing a more complex introspection.

--- src/lib/ui-components/ResponsiveTable.svelte
+++ src/lib/ui-components/ResponsiveTable.svelte
@@ -5,43 +5,48 @@
    * elements MUST be uniform with the first, or at least have values corresponding to the compatible properties in that first record. */
   export let data: Record<string, any>[]
   export let caption: string
-  /** Whether to sort props with sub-props last for nesting on separate rows after all single value props have been displayed on a common row. */
-  export let nestedLast: boolean = !true
+  /** Whether to sort nestable props to the end of the record and nest them on their own rows after all "simple" value props have been displayed on a common row. */
+  export let nesting: boolean = !true
   /** Pass in any special transfrom functions, keyed by the property names in your data, that generate html based on the element passed in. Only used
    * on data rows. Useful for turning data elements into hyperlinks or buttons in the table. */
   export let transforms: Record<string, (e: any) => any> = {}
   /** Pass in any special sort handling functions, keyed by the property names in your data. */
   export let sortings: Record<string, (a: any, b: any) => number> = {}
+  /** Optional function bind to return an array of property names to nest on their own row.
+   * Useful for overriding default behavior of doing so for all objects and arrays when nesting is enabled. */
+  export let getNestingKeys: (data: Record<string, any>[]) => string[] = (data: Record<string, any>[]) => {
+    const meta = getMetaData(data[0])
+    const nestingDefaultTypes = new Set(['object', 'array'])
+    return meta.filter(m => nestingDefaultTypes.has(m.type)).map(m => m.key)
+  }
 
   interface PropMeta { key: string, type: string }
   type EnhancedTypes = 'string' | 'number' | 'bigint' | 'boolean' | 'symbol' | 'undefined' | 'object' | 'function' | 'array'
 
-  const headingData = getMetaData(data[0])
   const incompatibleTypes = new Set(['undefined', 'function', 'symbol'])
   const arithmeticTypes = new Set(['number', 'bigint', 'boolean'])
-  const nestingTypes = new Set(['object', 'array'])
+  const nestingKeys = new Set(...(nesting ? getNestingKeys(data) : []))
 
-  const plainHeadings = headingData.filter(h => !nestingTypes.has(h.type))
-  const nestHeadings = headingData.filter(h => nestingTypes.has(h.type))
+  const headingMeta = sortByNesting(getMetaData(data[0]))
+  const plainMeta = headingMeta.filter(h => !nestingKeys.has(h.key))
+  const nestedMeta = headingMeta.filter(h => nestingKeys.has(h.key))
 
   /** Returns an array of { key, type } records that describe the properties of the `obj` that are compatible with display in this table. */
   function getMetaData (obj: any) {
     const keys = Object.keys(obj)
-    const filtered = keys.reduce<PropMeta[]>((wanted, key) => {
+    return keys.reduce<PropMeta[]>((wanted, key) => {
       let type: EnhancedTypes = typeof obj[key]
       if (type === 'object' && obj[key].length !== undefined) type = 'array'
       if (!incompatibleTypes.has(type)) wanted.push({ key, type })
       return wanted
     }, [])
-    /** Properties with arrays and objects at the bottom if `nestedLast` is desired . */
-    const orderedKeys = !nestedLast
-      ? filtered
-      : filtered.sort((a, b) => {
-        const aType = a.type === 'object' ? 1 : 0
-        const bType = b.type === 'object' ? 1 : 0
-        return aType - bType
-      })
-    return orderedKeys
+  }
+
+  /** Sorts `meta` by meta.key exisiting in `nestingKeys` if `nesting` is enabled. */
+  function sortByNesting (meta: PropMeta[]) {
+    return !nesting
+      ? meta
+      : meta.sort((a, b) => { return Number(nestingKeys.has(a.key)) - Number(nestingKeys.has(b.key)) })
   }
 
   let ascending = true
@@ -89,95 +94,68 @@
     <caption>{caption}</caption>
     {/if}
     <thead>
-      {#if nestedLast && nestHeadings.length > 0} <!-- Spreading record over multiple rows. -->
+      <tr>
+      {#each plainMeta as plainHead}
+        <th
+          class:selected={plainHead.key === selectedHeading}
+          aria-sort={plainHead.key !== selectedHeading ? 'none' : (ascending) ? 'ascending' : 'descending'}
+          on:click={() => { sortByHeading(plainHead) }}>{plainHead.key.replace('_', ' ')}
+          <span hidden={plainHead.key !== selectedHeading} class='order-icon'>{@html ascending ? '&#9661;' : '&#9651;'}</span>
+        </th>
+      {/each}
+      </tr>
+      {#each nestedMeta as nestedHead}
         <tr>
-        {#each plainHeadings as plain}
           <th
-            class:selected={plain.key === selectedHeading}
-            aria-sort={plain.key !== selectedHeading ? 'none' : (ascending) ? 'ascending' : 'descending'}
-            on:click={() => { sortByHeading(plain) }}>{plain.key.replace('_', ' ')}
-            <span hidden={plain.key !== selectedHeading} class='order-icon'>{@html ascending ? '&#9661;' : '&#9651;'}</span>
+            colspan={plainMeta.length ?? 1}
+            class:selected={nestedHead.key === selectedHeading}
+            aria-sort={nestedHead.key !== selectedHeading ? 'none' : (ascending) ? 'ascending' : 'descending'}
+            on:click={() => { sortByHeading(nestedHead) }}>{nestedHead.key.replace('_', ' ')}
+            <span hidden={nestedHead.key !== selectedHeading} class='order-icon'>{@html ascending ? '&#9661;' : '&#9651;'}</span>
           </th>
-        {/each}
         </tr>
-        {#each nestHeadings as nested}
-          <tr>
-            <th
-              colspan={plainHeadings.length ?? 1}
-              class:selected={nested.key === selectedHeading}
-              aria-sort={nested.key !== selectedHeading ? 'none' : (ascending) ? 'ascending' : 'descending'}
-              on:click={() => { sortByHeading(nested) }}>{nested.key.replace('_', ' ')}
-              <span hidden={nested.key !== selectedHeading} class='order-icon'>{@html ascending ? '&#9661;' : '&#9651;'}</span>
-            </th>
-          </tr>
-        {/each}
-      {:else} <!-- No multi-row representation of record data. -->
-        <tr>
-        {#each headingData as heading}
-          <th
-            class:selected={heading.key === selectedHeading}
-            aria-sort={heading.key !== selectedHeading ? 'none' : (ascending) ? 'ascending' : 'descending'}
-            on:click={() => { sortByHeading(heading) }}>{heading.key.replace('_', ' ')}
-            {#if heading.key === selectedHeading}
-            <span class='order-icon'>{@html ascending ? '&#9661;' : '&#9651;'}</span>
-            {/if}
-          </th>
-        {/each}
-        </tr>
-      {/if}
+      {/each}
     </thead>
     <tbody>
     {#each data as record, i}
-      {#if nestedLast && nestHeadings.length > 0} <!-- Spreading record over multiple rows. -->
-        <tr class:opaqued={opaqued(i)}>
-        {#each plainHeadings as plain}
-          <td data-cell={plain.key.replace('_', ' ')}>
-            {@html format(plain, record[plain.key])}
-          </td>
-        {/each}
-        </tr>
-        {#each nestHeadings as nested}
-        <tr class:opaqued={opaqued(i)}>
-          {#if nested.type === 'array'}
-            <td data-cell={nested.key.replace('_', ' ')}
-              colspan={plainHeadings.length ?? 1}>
-              <div class='array-container'>
-              {#each record[nested.key] as element}
-                <div>
-                  {@html format(nested, element)}
-                </div>
-              {/each}
-              </div>
-            </td>
-          {:else}
-            <td data-cell={nested.key.replace('_', ' ')}
-              colspan={plainHeadings.length ?? 1}>
-              <div class='flex-container'>
-                {@html format(nested, record[nested.key])}
-              </div>
-            </td>
-          {/if}
-        </tr>
-        {/each}
-      {:else} <!-- No multi-row representation of record data. -->
       <tr class:opaqued={opaqued(i)}>
-      {#each headingData as plain}
-        <td data-cell={plain.key.replace('_', ' ')}>
-          {#if plain.type === 'array'}
-            <div>
-              {#each record[plain.key] as element}
-              <div>
-                {@html format(plain, element)}
-              </div>
-              {/each}
-            </div>
-          {:else}
-            {@html format(plain, record[plain.key])}
-          {/if}
+      {#each plainMeta as plainData}
+        <td data-cell={plainData.key.replace('_', ' ')}>
+        {#if plainData.type === 'array'}
+          {#each record[plainData.key] as element}
+          <div>
+            {@html format(plainData, element)}
+          </div>
+          {/each}
+        {:else}
+          {@html format(plainData, record[plainData.key])}
+        {/if}
         </td>
       {/each}
       </tr>
-      {/if}
+      {#each nestedMeta as nestedData}
+      <tr class:opaqued={opaqued(i)}>
+        {#if nestedData.type === 'array'}
+          <td data-cell={nestedData.key.replace('_', ' ')}
+            colspan={plainMeta.length ?? 1}>
+            <div class='array-container'>
+            {#each record[nestedData.key] as element}
+              <div>
+                {@html format(nestedData, element)}
+              </div>
+            {/each}
+            </div>
+          </td>
+        {:else}
+          <td data-cell={nestedData.key.replace('_', ' ')}
+            colspan={plainMeta.length ?? 1}>
+            <div class='flex-container'>
+              {@html format(nestedData, record[nestedData.key])}
+            </div>
+          </td>
+        {/if}
+      </tr>
+      {/each}
     {/each}
     </tbody>
   </table>
