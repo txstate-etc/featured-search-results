34ce2dfff27bb34eacc24ff6cd7cffd85a289cb6
Drew Pilling (aap64) <Drew.Pilling@TxState.edu>
2023-10-24 10:03:28 -0500

    ResponsiveTable now supports sorting on all compatible types and allows for custom sorting options to be passed per data property key. - Updating styling on rows containing array elements.

--- src/lib/ui-components/ResponsiveTable.svelte
+++ src/lib/ui-components/ResponsiveTable.svelte
@@ -1,29 +1,26 @@
 <script lang=ts>
-  /** A responsive table that will generate itself based on the `data` and behavior parameters passed to it. */
-
-  /* TODO: Add a custom sorting mapping similar to transforms that allows a custom sort algorithm to be used.
-           - When done make sure to add a check to sortableTypes to include. Perhaps replace sortableTypes with sortable
-             fields.
-   */
-
-  interface PropMeta { key: string, type: string }
-  type EnhancedTypes = 'string' | 'number' | 'bigint' | 'boolean' | 'symbol' | 'undefined' | 'object' | 'function' | 'array'
-  const incompatibleTypes = new Set(['undefined', 'function', 'symbol'])
-  const arithmeticTypes = new Set(['number', 'bigint', 'boolean'])
-  const sortableTypes = new Set([...arithmeticTypes, 'string', 'array'])
-  const nestingTypes = new Set(['object', 'array'])
+  /** A responsive table that will generate itself based on the `data` and behavior bindings passed to it. */
 
   /** Array of `Record<string, any>[]` records to generate a table for. Uses the first element to figure out the shape of the data. All
    * elements MUST be uniform with the first, or at least have values corresponding to the compatible properties in that first record. */
   export let data: Record<string, any>[]
   export let caption: string
   /** Whether to sort props with sub-props last for nesting on separate rows after all single value props have been displayed on a common row. */
-  export let nestedLast: boolean = false
-  /** Pass in any special transfrom functions, keyed by the properties in your data, that generate html based on the element passed in. Only used
+  export let nestedLast: boolean = !true
+  /** Pass in any special transfrom functions, keyed by the property names in your data, that generate html based on the element passed in. Only used
    * on data rows. Useful for turning data elements into hyperlinks or buttons in the table. */
   export let transforms: Record<string, (e: any) => any> = {}
+  /** Pass in any special sort handling functions, keyed by the property names in your data. */
+  export let sortings: Record<string, (a: any, b: any) => number> = {}
+
+  interface PropMeta { key: string, type: string }
+  type EnhancedTypes = 'string' | 'number' | 'bigint' | 'boolean' | 'symbol' | 'undefined' | 'object' | 'function' | 'array'
 
   const headingData = getMetaData(data[0])
+  const incompatibleTypes = new Set(['undefined', 'function', 'symbol'])
+  const arithmeticTypes = new Set(['number', 'bigint', 'boolean'])
+  const nestingTypes = new Set(['object', 'array'])
+
   const plainHeadings = headingData.filter(h => !nestingTypes.has(h.type))
   const nestHeadings = headingData.filter(h => nestingTypes.has(h.type))
 
@@ -51,16 +48,20 @@
   let selectedHeading = ''
   /** Sorts `data` by the heading { key, type } data associated with the records in it. Array properties are sorted by their length.
    * Also handles updating what heading is selected for soriting and toggling asc/desc. */
-  function sortByHeading (heading: PropMeta) {
-    if (heading.key !== selectedHeading) { // Reset column state and resort.
-      selectedHeading = heading.key
+  function sortByHeading (meta: PropMeta) {
+    if (meta.key !== selectedHeading) { // Reset column state and resort.
+      selectedHeading = meta.key
       ascending = true
-      if (arithmeticTypes.has(heading.type)) {
-        data = data.sort((a, b) => { return a[heading.key] - b[heading.key] })
-      } else if (heading.type === 'string') {
-        data = data.sort((a, b) => { return a[heading.key].localeCompare(b[heading.key], undefined, { sensitivity: 'accent' }) })
-      } else { // Sort by length of each array.
-        data = data.sort((a, b) => { return a[heading.key].length - b[heading.key].length })
+      if (sortings[meta.key]) {
+        data = data.sort((a, b) => { return sortings[meta.key](a[meta.key], b[meta.key]) })
+      } else if (arithmeticTypes.has(meta.type)) {
+        data = data.sort((a, b) => { return a[meta.key] - b[meta.key] })
+      } else if (meta.type === 'string') {
+        data = data.sort((a, b) => { return a[meta.key].localeCompare(b[meta.key], undefined, { sensitivity: 'accent' }) })
+      } else if (meta.type === 'array') { // Sort by length of each array.
+        data = data.sort((a, b) => { return a[meta.key].length - b[meta.key].length })
+      } else { // Sort by number of keys in an object.
+        data = data.sort((a, b) => { return Object.keys(a[meta.key]).length - Object.keys(b[meta.key]).length })
       }
     } else {
       ascending = !ascending
@@ -72,9 +73,11 @@
     return (i % 2) > 0
   }
 
-  function format (heading: PropMeta, obj: any) {
-    if (transforms[heading.key]) return transforms[heading.key](obj)
-    else if (heading.type === 'object') return JSON.stringify(obj)
+  /** Formats `obj` based on any transformations passed for its associated heading type, OR calls JSON.stringify if typeof `object` and no transforms are defined.
+   * Else it just returns the object value without altering it. */
+  function format (meta: PropMeta, obj: any) {
+    if (transforms[meta.key]) return transforms[meta.key](obj)
+    else if (meta.type === 'object') return JSON.stringify(obj)
     return obj
   }
 
@@ -89,45 +92,29 @@
       {#if nestedLast && nestHeadings.length > 0} <!-- Spreading record over multiple rows. -->
         <tr>
         {#each plainHeadings as plain}
-          {#if sortableTypes.has(plain.type)}
           <th
-            class:sortable={true}
             class:selected={plain.key === selectedHeading}
             aria-sort={plain.key !== selectedHeading ? 'none' : (ascending) ? 'ascending' : 'descending'}
             on:click={() => { sortByHeading(plain) }}>{plain.key.replace('_', ' ')}
-            {#if plain.key === selectedHeading}
-            <span class='order-icon'>{@html ascending ? '&#9661;' : '&#9651;'}</span>
-            {/if}
+            <span hidden={plain.key !== selectedHeading} class='order-icon'>{@html ascending ? '&#9661;' : '&#9651;'}</span>
           </th>
-          {:else}
-          <th>{plain.key.replace('_', ' ')}</th>
-          {/if}
         {/each}
         </tr>
         {#each nestHeadings as nested}
           <tr>
-          {#if sortableTypes.has(nested.type)}
-          <th
-            colspan={plainHeadings.length ?? 1}
-            class:sortable={true}
-            class:selected={nested.key === selectedHeading}
-            aria-sort={nested.key !== selectedHeading ? 'none' : (ascending) ? 'ascending' : 'descending'}
-            on:click={() => { sortByHeading(nested) }}>{nested.key.replace('_', ' ')}
-            {#if nested.key === selectedHeading}
-            <span class='order-icon'>{@html ascending ? '&#9661;' : '&#9651;'}</span>
-            {/if}
-          </th>
-          {:else}
-          <th colspan={plainHeadings.length ?? 1}>{nested.key.replace('_', ' ')}</th>
-          {/if}
+            <th
+              colspan={plainHeadings.length ?? 1}
+              class:selected={nested.key === selectedHeading}
+              aria-sort={nested.key !== selectedHeading ? 'none' : (ascending) ? 'ascending' : 'descending'}
+              on:click={() => { sortByHeading(nested) }}>{nested.key.replace('_', ' ')}
+              <span hidden={nested.key !== selectedHeading} class='order-icon'>{@html ascending ? '&#9661;' : '&#9651;'}</span>
+            </th>
           </tr>
         {/each}
       {:else} <!-- No multi-row representation of record data. -->
         <tr>
         {#each headingData as heading}
-          {#if sortableTypes.has(heading.type)}
           <th
-            class:sortable={true}
             class:selected={heading.key === selectedHeading}
             aria-sort={heading.key !== selectedHeading ? 'none' : (ascending) ? 'ascending' : 'descending'}
             on:click={() => { sortByHeading(heading) }}>{heading.key.replace('_', ' ')}
@@ -135,9 +122,6 @@
             <span class='order-icon'>{@html ascending ? '&#9661;' : '&#9651;'}</span>
             {/if}
           </th>
-          {:else}
-          <th>{heading.key.replace('_', ' ')}</th>
-          {/if}
         {/each}
         </tr>
       {/if}
@@ -157,7 +141,7 @@
           {#if nested.type === 'array'}
             <td data-cell={nested.key.replace('_', ' ')}
               colspan={plainHeadings.length ?? 1}>
-              <div class='flex-container'>
+              <div class='array-container'>
               {#each record[nested.key] as element}
                 <div>
                   {@html format(nested, element)}
@@ -211,9 +195,6 @@
   .selected {
     font-weight: 700;
   }
-  .sortable {
-    cursor: pointer;
-  }
   .flex-container {
     display: flex;
     flex-flow: row wrap;
@@ -223,6 +204,9 @@
   :global(.flex-container > *) {
     flex-grow: 1;
     padding: 0.3rem;
+  }
+  .array-container {
+    column-count: 3;
   }
   table {
     width: 100%;
@@ -237,10 +221,12 @@
   caption {
     font-size: 1.5rem;
     font-weight: 700;
+    text-transform: capitalize;
   }
   th {
     background: hsl(0 0% 0% / 0.5);
     text-transform: capitalize;
+    cursor: pointer;
   }
   tr {
     border-bottom: 1px solid hsl(0 0% 0% / 0.05);
@@ -266,6 +252,9 @@
       content: attr(data-cell) ': ';
       font-weight: 700;
       text-transform: capitalize;
+    }
+    .array-container {
+      column-count: 2;
     }
   }
 </style>
