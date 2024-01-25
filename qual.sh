21cdd219bbccc3fa481d98ec94baad9268c89524
Drew Pilling (aap64) <Drew.Pilling@TxState.edu>
2024-01-25 15:06:25 -0600

    Final updates to build/push script.

--- qual.sh
+++ qual.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
-# Pass version as first argument, defaults to git branch name or git tag if that's not available.
-VER="${1:-`git symbolic-ref -q --short HEAD || git describe --tags --exact-match`}"
+# Pass version as first argument - REQUIRED.
+#VER="${1:-`git symbolic-ref -q --short HEAD || git describe --tags --exact-match`}"
+VER=$1
 GITBRANCH=`git symbolic-ref -q --short HEAD`
 GITTAG=`git describe --tags --exact-match 2>/dev/null`
 COMMIT=$(git rev-parse HEAD)
-if [[ "$GITTAG" != "" ]]; then
-  echo "Not overwriting existing tag $GITTAG on commit $COMMIT with new tag $VER."
+if [ "$VER" = "$GITBRANCH" ]; then
+  echo "Not tagging commit $COMMIT on branch $GITBRANCH with tag $GITBRANCH. Please provide a versioning argument."
   echo "Aborting build to preserve build tag integrity with commits."
   exit 1
 fi
-if [ "$VER" = "$GITBRANCH" ]; then
-  echo "Not tagging commit $COMMIT on branch $GITBRANCH with tag $VER."
+if [[ "$GITTAG" != "" && "$GITTAG" != "$VER" ]]; then
+  echo "Not overwriting existing tag $GITTAG on commit $COMMIT with new tag $VER."
   echo "Aborting build to preserve build tag integrity with commits."
   exit 1
 fi
 if [ "$VER" = "$GITTAG" ]; then
   echo "Commit $COMMIT is already tagged as $GITTAG."
-  echo "Aborting build to preserve build tag integrity with commits."
-  exit 1
-fi
-if `git tag $VER $COMMIT`; then
-  echo "Tagged commit $COMMIT on branch $GITBRANCH with tag $VER."
-  echo "Proceeding with versioned builds."
 else
-  echo "Failed to tag commit $COMMIT on branch $GITBRANCH with tag $VER."
-  echo "Aborting build to preserve build tag integrity with commits."
-  exit 1
+  if `git tag $VER $COMMIT`; then
+    echo "Tagged commit $COMMIT on branch $GITBRANCH with tag $VER."
+  else
+    echo "Failed to tag commit $COMMIT on branch $GITBRANCH with tag $VER."
+    echo "Aborting build to preserve build tag integrity with commits."
+    exit 1
+  fi
 fi
+echo "Proceeding with versioned builds."
 docker build -t registry.its.txstate.edu/search-featured-results .
 docker tag registry.its.txstate.edu/search-featured-results registry.its.txstate.edu/search-featured-results:$VER
 docker push registry.its.txstate.edu/search-featured-results
