ed63eecb291687c30d8c9c13808b3d32767a5520
Drew Pilling (aap64) <Drew.Pilling@TxState.edu>
2024-01-25 14:52:14 -0600

    Further updates to build/push script.

--- qual.sh
+++ qual.sh
@@ -1,21 +1,38 @@
 #!/bin/sh
 # Pass version as first argument, defaults to git branch name or git tag if that's not available.
-docker build -t registry.its.txstate.edu/search-featured-results .
-docker push registry.its.txstate.edu/search-featured-results
-docker build -t registry.its.txstate.edu/search-featured-results-cron -f Dockerfile.cron .
-docker push registry.its.txstate.edu/search-featured-results-cron
 VER="${1:-`git symbolic-ref -q --short HEAD || git describe --tags --exact-match`}"
 GITBRANCH=`git symbolic-ref -q --short HEAD`
-GITTAG=`git describe --tags --exact-match`
+GITTAG=`git describe --tags --exact-match 2>/dev/null`
 COMMIT=$(git rev-parse HEAD)
-if [ "$2" = "tag" ]; then
-  if [ "$GITTAG" = "$VER" ]; then
-    echo "Not tagging commit $COMMIT on branch $GITBRANCH with existing tag $GITTAG as new tag $VER"
-    exit 0
-  fi
-  echo "Tagging commit $COMMIT on branch $GITBRANCH with tag $VER"
-  git tag $VER $COMMIT
-  git push origin --tags
-else
-  echo "Not tagging commit $COMMIT on branch $GITBRANCH with existing tag $GITTAG as new tag $VER"
+if [[ "$GITTAG" != "" ]]; then
+  echo "Not overwriting existing tag $GITTAG on commit $COMMIT with new tag $VER."
+  echo "Aborting build to preserve build tag integrity with commits."
+  exit 1
 fi
+if [ "$VER" = "$GITBRANCH" ]; then
+  echo "Not tagging commit $COMMIT on branch $GITBRANCH with tag $VER."
+  echo "Aborting build to preserve build tag integrity with commits."
+  exit 1
+fi
+if [ "$VER" = "$GITTAG" ]; then
+  echo "Commit $COMMIT is already tagged as $GITTAG."
+  echo "Aborting build to preserve build tag integrity with commits."
+  exit 1
+fi
+if `git tag $VER $COMMIT`; then
+  echo "Tagged commit $COMMIT on branch $GITBRANCH with tag $VER."
+  echo "Proceeding with versioned builds."
+else
+  echo "Failed to tag commit $COMMIT on branch $GITBRANCH with tag $VER."
+  echo "Aborting build to preserve build tag integrity with commits."
+  exit 1
+fi
+docker build -t registry.its.txstate.edu/search-featured-results .
+docker tag registry.its.txstate.edu/search-featured-results registry.its.txstate.edu/search-featured-results:$VER
+docker push registry.its.txstate.edu/search-featured-results
+docker push registry.its.txstate.edu/search-featured-results:$VER
+docker build -t registry.its.txstate.edu/search-featured-results-cron -f Dockerfile.cron .
+docker tag registry.its.txstate.edu/search-featured-results-cron registry.its.txstate.edu/search-featured-results-cron:$VER
+docker push registry.its.txstate.edu/search-featured-results-cron
+docker push registry.its.txstate.edu/search-featured-results-cron:$VER
+git push origin --tags
