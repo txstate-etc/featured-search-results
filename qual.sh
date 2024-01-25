042a4ad0df5be3a3d5fc303723666cec3e8d816a
Drew Pilling (aap64) <Drew.Pilling@TxState.edu>
2024-01-25 14:04:08 -0600

    Updating QUAL build and push script.

--- qual.sh
+++ qual.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 # Pass version as first argument, defaults to git branch name or git tag if that's not available.
+docker build -t registry.its.txstate.edu/search-featured-results .
+docker push registry.its.txstate.edu/search-featured-results
+docker build -t registry.its.txstate.edu/search-featured-results-cron -f Dockerfile.cron .
+docker push registry.its.txstate.edu/search-featured-results-cron
 VER="${1:-`git symbolic-ref -q --short HEAD || git describe --tags --exact-match`}"
-docker build -t registry.its.txstate.edu/search-featured-results:$VER .
-docker push registry.its.txstate.edu/search-featured-results:$VER
-docker build -t registry.its.txstate.edu/search-featured-results-cron:$VER -f Dockerfile.cron .
-docker push registry.its.txstate.edu/search-featured-results-cron:$VER
+GITBRANCH=`git symbolic-ref -q --short HEAD`
+GITTAG=`git describe --tags --exact-match`
+COMMIT=$(git rev-parse HEAD)
 if [ "$2" = "tag" ]; then
-  COMMIT=$(git rev-parse HEAD)
-  echo "Tagging commit $COMMIT as tag $VER"
+  if [ "$GITTAG" = "$VER" ]; then
+    echo "Not tagging commit $COMMIT on branch $GITBRANCH with existing tag $GITTAG as new tag $VER"
+    exit 0
+  fi
+  echo "Tagging commit $COMMIT on branch $GITBRANCH with tag $VER"
   git tag $VER $COMMIT
   git push origin --tags
 else
-  echo "Not tagging commit."
+  echo "Not tagging commit $COMMIT on branch $GITBRANCH with existing tag $GITTAG as new tag $VER"
 fi
